c90eac947d7bef41eca58eb9b23839ca29a88e43
msandim <migsandim@hotmail.com>
2016-04-14 03:41:07 +0100

    ggsave (because preview in RStudio is broken), updated pdfs

--- scripts/descriptives_statistics.R
+++ scripts/descriptives_statistics.R
@@ -48,13 +48,16 @@ frequenciesSubject <- table(preprocessed_data$subject_matter)
 
 frequenciesCandidates <- as.data.frame(frequenciesCandidates)
 colnames(frequenciesCandidates)[1] <- "Candidate"
-frequenciesCandidates <- frequenciesCandidates[-c(1), ]
+frequenciesCandidates <- frequenciesCandidates[-1, ]
+
+levels(frequenciesCandidates$Candidate) <- gsub(" ", "\n", levels(frequenciesCandidates$Candidate))
 
 library(ggplot2)
 ggplot(data=frequenciesCandidates, aes(x=Candidate, y=Freq, fill=Candidate)) +
   geom_bar(stat="identity") +
   guides(fill=FALSE) +
-  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust = -0.5)
+  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust = -0.5) + ylab("Number of tweets")
+ggsave("plots/candidate_frequencies.pdf", width=8, height=5)
 
 ##########################################
 #      Plots subject
@@ -62,10 +65,13 @@ ggplot(data=frequenciesCandidates, aes(x=Candidate, y=Freq, fill=Candidate)) +
 
 frequenciesSubject <- as.data.frame(frequenciesSubject)
 colnames(frequenciesSubject)[1] <- "Subject"
-frequenciesSubject <- frequenciesSubject[-c(1), ]
+frequenciesSubject <- frequenciesSubject[-1, ]
+
+levels(frequenciesSubject$Subject) <- gsub(" ", "\n", levels(frequenciesSubject$Subject))
 
 library(ggplot2)
 ggplot(data=frequenciesSubject, aes(x=Subject, y=Freq, fill=Subject)) +
   geom_bar(stat="identity") +
   guides(fill=FALSE) +
-  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust = -0.5)
+  geom_text(aes(label=Freq), position=position_dodge(width=0.9), vjust = -0.5) + ylab("Number of tweets")
+ggsave("plots/subject_frequencies.pdf", width=8, height=5)
